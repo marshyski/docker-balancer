d773ca3adba02f849131abd0f6ba8d2fd8299392
marshyski <marshyski@gmail.com>
2015-07-23 14:15:56 -0400

    Added max cpu/container threshold

--- __init__.py
+++ __init__.py
@@ -4,7 +4,6 @@ Docker Balancer
 from flask import Flask, request, jsonify, Response
 from flask_limiter import Limiter
 from werkzeug.contrib.fixers import ProxyFix
-from operator import itemgetter
 from json import dumps
 from os import getcwd
 import redis
@@ -12,8 +11,8 @@ import yaml
 
 config_file = getcwd() + '/' + 'config.yaml'
 config_yaml = yaml.load(file(config_file, 'r'))
-min_threshold = int(config_yaml['min_threshold'])
-max_threshold = int(config_yaml['max_threshold'])
+max_container_threshold = int(config_yaml['max_container_threshold'])
+max_cpu_threshold = int(config_yaml['max_cpu_threshold'])
 redis_host = config_yaml['redis_host']
 redis_port = config_yaml['redis_port']
 redis_db = config_yaml['redis_db']
@@ -26,14 +25,26 @@ def service_func(service):
     """Processing dictionary for return to flask route"""
     lowest_host = {}
     maxed_host = {}
+    all_hosts = []
     keys = db.keys('*')
 
     for key in keys:
-        val = int(db.get(key))
-        if val >= max_threshold:
-            maxed_host[key] = val
-        else:
-            lowest_host[key] = val
+        value = db.get(key)
+        cpu = int(value.split(' ', 1)[0])
+        count = int(value.split(' ', 1)[1])
+        all_hosts.append(key)
+
+        if cpu >= max_cpu_threshold:
+            maxed_host[key] = 'CPU=' + str(cpu)
+
+        if count >= max_container_threshold:
+            maxed_host[key] = 'Containers=' + str(count)
+
+        if cpu < max_cpu_threshold and key not in maxed_host:
+            lowest_host[key] = cpu
+
+        if count < max_container_threshold and key not in maxed_host:
+            lowest_host[key] = count
 
     min_host = min(lowest_host, key=lowest_host.get)
     max_host = max(lowest_host, key=lowest_host.get)
@@ -47,28 +58,28 @@ def service_func(service):
         return max_host
 
     if service == 'all':
-        return str(sorted(join_dicts.items(), key=itemgetter(1)))
+        return str(sorted(all_hosts))
 
     if service == 'total':
         return str(len(join_dicts))
 
     if service == 'available':
-        return str(sorted(lowest_host.items(), key=itemgetter(1)))
+        return str(sorted(lowest_host.keys()))
 
     if service == 'available-total':
         return str(len(lowest_host))
 
     if service == 'unavailable':
-        return str(sorted(maxed_host.items(), key=itemgetter(1)))
+        return str(sorted(maxed_host.keys()))
 
     if service == 'unavailable-total':
         return str(len(maxed_host))
 
     if service == 'stats':
-        all_array = dumps(join_dicts, ensure_ascii=False, sort_keys=True)
+        all_array = dumps(all_hosts, ensure_ascii=False, sort_keys=True)
         lowest = min_host
         highest = max_host
-        available = dumps(lowest_host, ensure_ascii=False, sort_keys=True)
+        available = dumps(lowest_host.keys(), ensure_ascii=False, sort_keys=True)
         available_total = len(lowest_host)
         unavailable = dumps(maxed_host, ensure_ascii=False, sort_keys=True)
         unavailable_total = len(maxed_host)
@@ -79,6 +90,7 @@ def service_func(service):
         "lowest": lowest, "total": total, "unavailable": unavailable, "unavailable_total": \
         unavailable_total}).replace("'", '"').replace('"{', '{').replace('}"', '}').replace('",', '",\n')\
         .replace('{"', '{\n "').replace("},", '\n },\n').replace(', "', ', \n "').replace('}}', '\n }\n}')\
+        .replace('"["', '[\n "').replace(']",', '\n ],')\
         , mimetype='application/json')
 
 @app.route('/api/<string:service>')
@@ -89,21 +101,11 @@ def get_service(service):
 @app.route('/api/<int:cpu>/<int:count>', methods=['POST'])
 def post_docker_info(cpu, count):
     """Post Docker host CPU utilization percent and docker container count"""
-    ip = request.remote_addr
+    ip_addr = request.remote_addr
+    value = "%s %s" % (cpu, count)
+    db.set(ip_addr, value)
 
-    if count > min_threshold:
-        count = min_threshold
-
-    post_dic = {'cpu': cpu, 'count': count}
-    list_min = min(post_dic, key=post_dic.get)
-    list_max = max(post_dic, key=post_dic.get)
-    item_max = post_dic.get(list_max)
-    item_min = post_dic.get(list_min)
-    score = item_max - item_min
-
-    db.set(ip, score)
-
-    return jsonify(docker_host=ip, cpu_percent=cpu, docker_count=count, score=score)
+    return jsonify(docker_host=ip, cpu_percent=cpu, docker_count=count)
 
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
